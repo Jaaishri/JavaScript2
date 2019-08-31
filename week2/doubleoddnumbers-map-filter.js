94f64f9c82c36f5d28bbb2c1aa542d3a8905ee8a
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2019-08-31 22:37:22 +0200

    Delete doubleoddnumbers-map-filter.js

--- week2/doubleoddnumbers-map-filter.js
+++
@@ -1,9 +0,0 @@
-var myNumbers = [1, 2, 3, 4];
-
-var newNumbers = myNumbers.filter(function(number){
-    return (number % 2 !== 0);
-}).map(function(number){
-    return number * 2;
-});
-
-console.log("The doubled numbers are", newNumbers);
