f494655aa7685ea22bfc67cd3212a18d63260bee
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2019-09-14 10:56:51 +0200

    Scope and Closures

---
+++ week3/step3.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function createBase(base) {
+    return base + 6;
+}
+
+const addSix = createBase(6);
+
+console.log(addSix(10)); // returns 16
+console.log(addSix(21)); // returns 27
+
+// Do not change or remove anything below this line
+module.exports = createBase;
