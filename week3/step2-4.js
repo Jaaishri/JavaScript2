1936e97cac079a263e2a16683d659cf63a7bc9fe
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2019-09-14 10:49:22 +0200

    constructors

---
+++ week3/step2-4.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function Dog() {
+  this.name = 'Carry';
+  this.color = 'black';
+  this.numLegs = 4;
+}
+
+const hound = new Dog();
+hound.name = 'Isabella';
+hound.color = 'Gray';
+hound.numLegs = 4;
+
+// Do not change or remove anything below this line
+module.exports = hound;
